467c91a7557aca2c42b62b4f64b2757a265ab17a
Josh Nichols <josh@technicalpickles.com>
2008-09-26 14:12:19 -0400

    Converted rubyflow config to new style.

--- config/sites/rubyflow.rb
+++ config/sites/rubyflow.rb
@@ -1,44 +1,46 @@
-config.site_title     = 'RubyFlow'
-config.default_title  = 'RubyFlow : Community Filtered News'
+configatron.site_title     = 'RubyFlow'
+configatron.default_title  = 'RubyFlow : Community Filtered News'
 
-config.sub_title = 'Community-maintained news'
+configatron.sub_title = 'Community-maintained news'
 
-config.namespace(:meta) do |meta|
-  meta.keywords = 'news, news site'
-  meta.author = 'RubyFlow'
-end
+configatron.meta.keywords = 'news, news site'
+configatron.meta.author = 'RubyFlow'
 
-config.language = 'en-us'
+configatron.language = 'en-us'
 
-config.site_url = 'http://rubyflow.local/'
-config.rss_url = 'http://rubyflow.local/items.rss'
+configatron.site_url = 'http://rubyflow.local/'
+configatron.rss_url = 'http://rubyflow.local/items.rss'
 
-config.namespace(:google_analytics) do |google_analytics|
-  google_analytics.enabled = false
-  google_analytics.account = 'UA-2237791-8'
-end
+configatron.google_analytics.enabled = false
+configatron.google_analytics.account = 'UA-2237791-8'
 
 # Set this to true for a more fancy interface, but for the cost of page load time
-config.namespace(:javascript) do |javascript|
-  javascript.enabled = false
-end
+configatron.javascript.enabled = false
 
-config.edit_expiration_in_minutes = 60
+configatron.edit_expiration_in_minutes = 60
 
-config.sidebar_redcloth = <<END
+configatron.sidebar_redcloth = <<END
+!http://www.engineyard.com/images/ey_banner_150x200.png!:http://www.engineyard.com/?utm_source=RubyFlow&utm_medium=Banner&utm_campaign=Sponsorship
+
+"Follow RubyFlow on **Twitter**":http://twitter.com/rubyflow
+
 h3. What?
 
-This site is intended for news gathering, with links chosen and summarised by the community.
+RubyFlow is a community driven Ruby links site. Items are not added automatically, but chosen and summarized by the community, resulting in a higher quality of links than "social bookmarking sites":http://ruby.reddit.com/.
+
+h3. International Versions
+
+**Russian:** "rubyflow.ru":http://rubyflow.ru/
+
+**Japanese:** There is "a Japanese version of RubyFlow":http://www.kuwata-lab.com/rubyflow-ja/ available, thanks to Makoto Kuwata!
+
+**Chinese:** "Chinese Rubyflow site":http://flow.rubynow.com/.
+
+**Dutch:** "Dutch (nederlandse) Rubyflow site":http://www.rubyweb.nl/.
 
 h3. How?
 
-Enjoy the links, comment, or "make a post":/items/new of your own. You don't need to be signed in!
-
-_Note: If you're not signed in, your byline is unlinked and your links are made "nofollow" to prevent spam. You also get CAPTCHAed!_
-
-h3. Who?
-
-RubyFlow was built by "Peter Cooper":http://peterc.org/ of "Ruby Inside":http://rubyinside.com, but is ultimately a community site.
+Enjoy the links, comment or "make a post of your own":http://www.rubyflow.com/items/new. You don't need to be signed in! Only posts made by users with a good track record make it into the feed though, so no spam.
 END
 
-config.sidebar = RedCloth.new(config.sidebar_redcloth).to_html
+configatron.sidebar = RedCloth.new(configatron.sidebar_redcloth).to_html
