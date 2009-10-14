c38b2162ad86d5594c2690f64aaf94322e52e661
Jerod Santo <jerod.santo@gmail.com>
2009-10-13 21:54:28 -0500

    pretty image links!

--- config/sites/cappuccinoflow.rb
+++ config/sites/cappuccinoflow.rb
@@ -18,21 +18,3 @@ configatron.google_analytics.account = 'UA-3287978-14'
 configatron.javascript.enabled = false
 
 configatron.edit_expiration_in_minutes = 60
-
-configatron.sidebar_redcloth = <<END
-h3. What?
-
-CappuccinoFlow is a community driven "Cappuccino":http://www.cappuccino.org link blog. Posts are by members of the community — like you!
-
-h3. How?
-
-Enjoy the links, comment, or "make a post":/items/new of your own. You don't need to be signed in!
-
-_Note: Only posts by registered members with 1 previously good post make it into the "RSS":http://feeds.feedburner.com/cappuccinoflow and "Twitter":http://twitter.com/cappuccinoflow feeds._
-
-h3. Who?
-
-CappuccinoFlow is a fork of RubyFlow (built by "Peter Cooper":http://peterc.org/). It is maintained by "Jerod Santo":http://jerodsanto.net/, but is ultimately a community site. "Contributions are welcome":http://github.com/sant0sk1/cappuccinoflow/
-END
-
-configatron.sidebar = RedCloth.new(configatron.sidebar_redcloth).to_html
