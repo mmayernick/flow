ba1813a096f67eb5b88f1c3dbed5c03ad375cd9a
Aaron Brethorst <aaron@brethorsting.com>
2011-11-17 14:22:13 -0800

    add bundler and rvm support

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -14,12 +14,6 @@ Rails::Initializer.run do |config|
   }
 
   config.active_record.default_timezone = :utc
-
-  config.gem 'RedCloth',      :version => '4.2.2',  :lib => 'redcloth'
-  config.gem 'will_paginate', :version => '2.2.0'
-  config.gem 'configatron',   :version => '2.5.1'
-  config.gem 'bitly',         :version => '0.3.1'
-  config.gem 'twitter',       :version => '0.9.8'
 
   config.active_record.observers = :item_observer
 end
