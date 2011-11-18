925c23769081da3ca55ba1261809b1bae6eb8e98
Aaron Brethorst <aaron@brethorsting.com>
2011-11-17 16:11:02 -0800

    upgrade application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Cappuccinoflow
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+    config.active_record.observers = :item_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -37,6 +37,6 @@ module Cappuccinoflow
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
