cabdbda9108fd5388a886caf8df8a6608fb8ad3f
Aaron Brethorst <aaron@brethorsting.com>
2011-11-17 16:44:53 -0800

    move app controller code to new controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def passes_captcha?
+    params[:captcha] && Digest::SHA1.hexdigest(params[:captcha].upcase.chomp)[0..5] == params[:captcha_guide]
+  end
+  
+  include AuthenticatedSystem
+  
+  def render_404
+    render :status => 404, :text => '404 Not Found'
+  end
+  
+  def render_403
+    render :status => 403, :text => '403 Forbidden'
+  end
+  
+  def config
+    configatron
+  end
+  
+  helper_method :config
 end
