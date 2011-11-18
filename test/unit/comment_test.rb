001a03f17faad5ef849bb72ac28de4c1cb0990c9
Aaron Brethorst <aaron@brethorsting.com>
2011-11-18 00:55:11 -0800

    comment spec

--- test/unit/comment_test.rb
+++
@@ -1,7 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-
-class CommentTest < ActiveSupport::TestCase
-  should belong_to :user
-  should belong_to :item
-  should ensure_length_of(:content).is_at_least(1).is_at_most(10000)
-end
