6f5e2b17c4a11228fbfa8d29c178006fb68de583
Aaron Brethorst <aaron@brethorsting.com>
2011-11-19 12:39:35 -0800

    sidebar hamlification, move some images, site config

--- config/sites/iosflow.rb
+++ config/sites/iosflow.rb
@@ -9,9 +9,10 @@ configatron.meta.author = 'iOSFlow'
 configatron.language = 'en-us'
 
 configatron.site_url = 'http://0.0.0.0:3000/'
-configatron.rss_url = 'http://0.0.0.0:3000/items.rss'
+configatron.rss_url = 'http://feeds.feedburner.com/cappuccinoflow'
+configatron.twitter_url = '#'
 
-configatron.google_analytics.enabled = false
+configatron.google_analytics.enabled = true
 configatron.google_analytics.account = 'UA-2237791-8'
 
 # Set this to true for a more fancy interface, but for the cost of page load time
@@ -21,25 +22,15 @@ configatron.edit_expiration_in_minutes = 60
 
 configatron.sidebar_redcloth = <<END
 
-"Follow RubyFlow on **Twitter**":http://twitter.com/rubyflow
+"Follow iOSFlow on **Twitter**":http://twitter.com/iosflow
 
 h3. What?
 
-RubyFlow is a community driven Ruby links site. Items are not added automatically, but chosen and summarized by the community, resulting in a higher quality of links than "social bookmarking sites":http://ruby.reddit.com/.
-
-h3. International Versions
-
-**Russian:** "rubyflow.ru":http://rubyflow.ru/
-
-**Japanese:** There is "a Japanese version of RubyFlow":http://www.kuwata-lab.com/rubyflow-ja/ available, thanks to Makoto Kuwata!
-
-**Chinese:** "Chinese Rubyflow site":http://flow.rubynow.com/.
-
-**Dutch:** "Dutch (nederlandse) Rubyflow site":http://www.rubyweb.nl/.
+iOSFlow is a community driven iOS links site.
 
 h3. How?
 
-Enjoy the links, comment or "make a post of your own":http://www.rubyflow.com/items/new. You don't need to be signed in! Only posts made by users with a good track record make it into the feed though, so no spam.
+Enjoy the links, comment or "make a post of your own":http://0.0.0.0:3000/items/new. You don't need to be signed in! Only posts made by users with a good track record make it into the feed though, so no spam.
 END
 
 configatron.sidebar = RedCloth.new(configatron.sidebar_redcloth).to_html
