efd2b7c6669d7357dd8e678093686fe4eb0a945d
Aaron Brethorst <aaron@brethorsting.com>
2011-11-19 18:14:10 -0800

    oops migration bug. all existing users will have api keys now.

--- db/migrate/20111120015051_add_api_key_to_users.rb
+++ db/migrate/20111120015051_add_api_key_to_users.rb
@@ -2,5 +2,11 @@ class AddApiKeyToUsers < ActiveRecord::Migration
   def change
     add_column :users, :api_key, :string
     add_index :users, :api_key, :unique => true
+    
+    User.reset_column_information
+    
+    User.all.each do |u|
+      u.save!
+    end
   end
 end
