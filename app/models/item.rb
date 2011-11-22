32f7552987052ad7a111ebef5d84c95f9036c9cb
Aaron Brethorst <aaron@brethorsting.com>
2011-11-22 15:12:00 -0800

    S3 for item image storage.

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,10 +21,10 @@ class Item < ActiveRecord::Base
 
   has_attached_file :image,
                     :styles => { :medium => "300x300>", :thumb => "100x100#" }
-                    # :storage => :s3,
-                    # :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
-                    # :path => ":attachment/:id/:style.:extension",
-                    # :bucket => "iosdev_#{Rails.env}"
+                    :storage => :s3,
+                    :s3_credentials => {access_key_id: ENV['AMAZON_ACCESS_KEY_ID'], secret_access_key: ENV['AMAZON_SECRET_ACCESS_KEY']},
+                    :path => ":attachment/:id/:style.:extension",
+                    :bucket => "iosdev_#{Rails.env}"
 
 
   before_save :anonymize_byline, :if => :anonymous?
