c962da4d1f9e5049a217f6d2cd0b1e92e807060d
Aaron Brethorst <aaron@brethorsting.com>
2011-11-23 14:42:47 -0800

    well, that didn't work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,7 @@ class ApplicationController < ActionController::Base
 
   def tweet(item)
     return unless Rails.env.production? && @item.tweetable?
-
-    auth = Twitter::OAuth.new(ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'])
-    auth.authorize_from_access(ENV['TWITTER_ACCESS_TOKEN'], ENV['TWITTER_ACCESS_SECRET'])
-    client = Twitter::Base.new(auth)
-
-    client.update("#{truncate(item.title, :length => 100)} #{item_url(item)}")
+    Twitter.update("#{truncate(item.title, :length => 100)} #{item_url(item)}")
   end
 
   def render_404
